96e421b92318f556836ef3a20be9fd0a41e4de66
YuPo13 <yuliya.povzlo@gmail.com>
2019-12-11 18:17:21 +0200

    Still have 2 questions

--- contacts.py
+++ contacts.py
@@ -1,12 +1,50 @@
+"""This script operates phonebook data operations"""
+import pickle
+
+
+def check_absence(func):
+    """Check if contact requested exists"""
+    def wrapper(self, name, *others):
+        if name not in self.contacts:
+            raise KeyError
+        return func(self, name, *others)
+    return wrapper
+
+
 class PhoneBook:
+    """This class represents object with standard crud features"""
+    def __init__(self):
+        """Initiate the contacts container"""
+        self.contacts = {}
+        self.filename = "phonebook.pickle"
+
+    def read_contacts_from_file(self):
+        """Open the pickle file that contains contacts inputs"""
+        try:
+            with open(self.filename, 'rb') as func:
+                self.contacts = pickle.load(func)
+        except FileNotFoundError:
+            self.contacts = {}
+
+    def write_to_file(self):
+        """Save the contacts data into the pickles file"""
+        with open(self.filename, 'wb') as func:
+            pickle.dump(self.contacts, func)
+
     def create(self, name, phone):
-        pass
+        """Add new contact key-value into contacts"""
+        self.contacts[name] = phone
 
     def read(self, name):
-        pass
+        """Find the contact requested and provide it"""
+        return self.contacts[name]
 
+    @check_absence
     def update(self, name, phone):
-        pass
+        """Check if contact requested exists and set new value for it"""
+        self.contacts[name] = phone
 
+    @check_absence
     def delete(self, name):
-        pass
+        """Delete required contact from the contacts"""
+        self.contacts.pop(name)
--- main.py
+++ main.py
@@ -1,14 +1,75 @@
+"""This script represents communication with program user"""
 import contacts
 
 
-while True:
+P_B = contacts.PhoneBook()
+
+
+def ask_to_do():
+    """Provide list of phonebook action options for user"""
     print("""What do you want to do?
-c - create
-r - read
-u - update
-d - delete
-q - quit
-""")
-    action = input("?").lower()
-    if action == 'q':
-        break
+    c - create
+    r - read
+    u - update
+    d - delete
+    q - quit
+    """)
+
+
+def confirm_action(text):
+    """Notify user of action executed"""
+    print(text)
+
+
+def main():
+    """Execute user's commands"""
+    while True:
+        ask_to_do()
+        action = input("?").lower()
+
+        if action == 'c':
+            name = input("Provide contact name:\n")
+            phone = input("Provide contact phone:\n")
+            P_B.create(name, phone)
+            confirm_action(f"New contact {name} has been added.")
+        elif action == 'q':
+            confirm_action("Goodbye!")
+            break
+        elif action == 'r':
+            try:
+                name = input("Provide contact name to search by:\n")
+                phone = P_B.read(name)
+                confirm_action(f"Contact found:\n Name: {name}\n "
+                               f"Phone: {phone}")
+            except KeyError:
+                confirm_action(f"Contact {name} is not found.")
+        elif action == 'u':
+            try:
+                name = input("Provide contact whose phone number "
+                             "should be updated:\n")
+                phone = input("Provide new phone value:\n")
+                P_B.update(name, phone)
+                confirm_action("Contact has been updated.")
+            except KeyError:
+                confirm_action(f"Contact {name} is not found.")
+        elif action == 'd':
+            try:
+                name = input("Provide contact to be deleted:\n")
+                P_B.delete(name)
+                confirm_action(f"Contact {name} is deleted.")
+            except KeyError:
+                confirm_action(f"Contact {name} is not found.")
+        else:
+            print("""Please choose one of options below:
+            c - create
+            r - read
+            u - update
+            d - delete
+            q - quit
+            """)
+
+
+if __name__ == "__main__":
+    P_B.read_contacts_from_file()
+    main()
+    P_B.write_to_file()
